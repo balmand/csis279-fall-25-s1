ebf542ac2fc764e2c4a242b341569672fcd78cfb
Bahaa <bahaaghemrawi12@hotmail.com>
2025-10-07 09:46:50 +0300

    added customer management

--- api/src/domain/entities/Customer.js
+++ api/src/domain/entities/Customer.js
@@ -1,13 +1,9 @@
-export class CustomerDTO {
+export class Customer {
     constructor({ id, name, email, phone, address }) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.address = address;
-    }
-
-    static fromEntity(entity){
-        return new CustomerDTO(entity);
     }
 }
